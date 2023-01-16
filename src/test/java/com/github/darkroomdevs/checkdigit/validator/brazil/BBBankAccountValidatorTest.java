5fdb8c00139a9422ff3893d67cbd2e4718a1a370
João Paulo Lima <jpaulo.on@gmail.com>
2023-01-16 11:30:09 -0300

    :white_check_mark: Add test for banco do Brasil validation.

---
+++ src/test/java/com/github/darkroomdevs/checkdigit/validator/brazil/BBBankAccountValidatorTest.java
@@ -0,0 +1,34 @@
+package com.github.darkroomdevs.checkdigit.validator.brazil;
+
+import com.github.darkroomdevs.checkdigit.validator.DigitValidator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BBBankAccountValidatorTest {
+
+    private DigitValidator digitValidator;
+
+    @BeforeEach
+    public void setup() {
+        digitValidator = BBBankAccountValidator.INSTANCE;
+    }
+
+    @Test
+    void assertThatBBBankAccountIsValid() {
+        assertThat(digitValidator.valid("11666153")).isTrue();
+        assertThat(digitValidator.valid("1248654X")).isTrue();
+        assertThat(digitValidator.valid("399477")).isTrue();
+    }
+
+    @Test
+    void assertThatBBBankAccountIsInvalid() {
+        assertThat(digitValidator.valid("")).isFalse();
+        assertThat(digitValidator.valid("           ")).isFalse();
+        assertThat(digitValidator.valid(null)).isFalse();
+        assertThat(digitValidator.valid("1193615Y")).isFalse();
+        assertThat(digitValidator.valid("76502982300")).isFalse();
+        assertThat(digitValidator.valid("11111111111")).isFalse();
+    }
+}
