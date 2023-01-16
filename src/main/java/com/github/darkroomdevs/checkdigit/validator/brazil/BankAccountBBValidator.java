e04b3060ebdc84baccd714e3a08c4eb55e9376ff
João Paulo Lima <jpaulo.on@gmail.com>
2023-01-16 13:42:53 -0300

    :recycle: refactoring the code.
    Co-authored-by: Alfredo Fialho <alfoltran@gmail.com>.

--- src/main/java/com/github/darkroomdevs/checkdigit/validator/brazil/BankAccountBBValidator.java
+++ src/main/java/com/github/darkroomdevs/checkdigit/validator/brazil/BankAccountBBValidator.java
@@ -2,9 +2,10 @@ package com.github.darkroomdevs.checkdigit.validator.brazil;
 
 import com.github.darkroomdevs.checkdigit.util.ModuloUtil;
 import com.github.darkroomdevs.checkdigit.validator.DigitValidator;
+import org.apache.commons.lang3.StringUtils;
 
 /**
- * This class checks whether a specific string represents a valid bank account of Banco do Brasil.
+ * This class checks whether a specific string represents a valid bank account number of Banco do Brasil.
  * <p/>
  * Reference: https://www.bb.com.br/docs/pub/emp/empl/dwn/Orientacoes.pdf
  */
@@ -16,26 +17,22 @@ public final class BankAccountBBValidator implements DigitValidator {
     }
 
     /**
-     * Checks whether the specified string represents a valid bank account of Banco do Brasil.
+     * Checks whether the specified string represents a valid bank account number of Banco do Brasil.
      *
      * @param bankAccount The sequence to verify.
-     * @return {@code true} if the sequence is a valid bankAccount; {@code false} otherwise.
+     * @return {@code true} if the sequence is a valid bank account number; {@code false} otherwise.
      */
     public boolean valid(String bankAccount) {
-        if (bankAccount == null) {
-            return false;
-        }
-
-        bankAccount = bankAccount.replace("X", "0");
-
-        if (!bankAccount.matches("\\d{1,9}")) {
+        if (StringUtils.isBlank(bankAccount)
+                || (StringUtils.length(bankAccount) > 9)
+                || !bankAccount.matches("\\d+[0-9X]")
+                || bankAccount.matches("0+|1+|2+|3+|4+|5+|6+|7+|8+|9+")) {
             return false;
         }
 
         int bankAccountSize = bankAccount.length();
-        String lastDigit = String.valueOf(bankAccount.charAt(bankAccountSize - 1));
-        String digits = bankAccount.substring(0, bankAccountSize - 1);
-
-        return ModuloUtil.compute(digits, bankAccountSize + 1).orElse("").equals(lastDigit);
+        char digit = bankAccount.charAt(bankAccountSize - 1);
+        return ModuloUtil.compute(bankAccount.substring(0, bankAccountSize - 1), bankAccountSize + 1).orElse("")
+                .equals(String.valueOf(digit == 'X' ? '0' : digit));
     }
 }
